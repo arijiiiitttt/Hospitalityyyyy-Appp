8b1b5a3612936c1e9a7412db320576b9cc994794
Arijit <arijit123roy098@gmail.com>
2025-04-06 10:03:37 +0530

    first init

---
+++ frontend/src/Components/MainPAGES/Website.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Website = () => {
+  return (
+   <>
+   <h1 className='text-lg'>helloworld</h1>
+   </>
+  )
+}
+
+export default Website
---
+++ frontend/src/app/page.js
@@ -0,0 +1,15 @@
+"use client"
+import Website from '@/Components/MainPAGES/Website'
+import React from 'react'
+
+
+
+const page = () => {
+  return (
+    <>
+    <Website/>
+    </>
+  )
+}
+
+export default page
